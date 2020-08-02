48b373b7d231bdc764b996b85775287ca23afd95
Shiina <shiinaliu@qq.com>
2020-08-02 15:20:52 +0800

    router bug

---
+++ 2020_08_02_Shiina/src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-container>
+        <el-header></el-header>
+        <el-main>
+            <router-view />
+        </el-main>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 2020_08_02_Shiina/src/views/Signin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        index=========
+        <router-link to='/'>去主页</router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
